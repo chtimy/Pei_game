7e9d738f80b86be43d393028ff9df5907e5d5eb6
Avzgui <contact.avzgui@gmail.com>
2018-11-04 14:53:35 +0100

    init music branch

--- scripts/stage.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-signal enter_stage
-
-func _on_TouchScreenButton2_pressed():
-	for area in $image/Player/Trigger.get_overlapping_areas():
-		print(area.get_name())
-		if area.get_name() == "kitchen_trigger":
-			emit_signal("enter_stage", 1, 1)
